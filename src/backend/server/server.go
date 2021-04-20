c113bb600e36816f02e7683c4d05c2b21dcf787f
Biel <bielp07@gmail.com>
2021-04-20 08:21:36 -0300

    Filter 'port' argument to avoid ACE.
    If the port argument inputted by the user, matches with the regexp
    filter, the program exits and does not executes the command.

--- src/backend/server/server.go
+++ src/backend/server/server.go
@@ -5,12 +5,31 @@ import (
 	"os"
 	"net/http"
 	"log"
+	"regexp"
 )
 
 var port string = ":8080"
+var pattern *regexp.Regexp
 
 func home(rw http.ResponseWriter, req *http.Request) {
 	fmt.Println("Home!")
+}
+
+/* Using regexp to match trials of Arbitrary
+ * Command Execution. If string matches,
+ * the program prints a message and exits,
+ * so the command does not get interpreted
+ * as a port argument.
+ */
+
+func filterArg(str string) string {
+	pattern = regexp.MustCompile(`[^\w@%+=:,./-]`)
+
+	if(pattern.MatchString(str)) {
+		fmt.Println("Easy bruh! No commands here!")
+		os.Exit(1)
+	}
+	return str
 }
 
 /* If no port number is specified after '-s',
@@ -22,10 +41,10 @@ func Start() {
 	if(len(os.Args) == 2) {
 		port = port
 	} else {
+		filterArg(os.Args[2])
 		port = ":" + os.Args[2]
 	}
-
+	fmt.Printf("Starting server at localhost%s\n", port)
 	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(port, nil))
-
 }
