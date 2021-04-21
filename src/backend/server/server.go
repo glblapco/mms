66c48d5e63dac1b28cd7174d4561adcfa280b2ef
Biel <bielp07@gmail.com>
2021-04-21 09:23:09 -0300

    Add error handling.
    Return an error if request page is different than "/" and return an
    error if request method is different than GET.

--- src/backend/server/server.go
+++ src/backend/server/server.go
@@ -11,31 +11,32 @@ import (
 var port string = ":8080"
 var pattern *regexp.Regexp
 
-func home(rw http.ResponseWriter, req *http.Request) {
-	fmt.Println("Home!")
-}
-
-/* Using regexp to match trials of Arbitrary
- * Command Execution. If string matches,
- * the program prints a message and exits,
- * so the command does not get interpreted
- * as a port argument.
+/* @@@(Biel A. P.): Using regexp to match trials 
+ * of Arbitrary Command Execution. If string 
+ * matches, the program prints a message and 
+ * exits, so the command does not get 
+ * interpreted as a port argument.
  */
 
 func filterArg(str string) string {
 	pattern = regexp.MustCompile(`[^\w@%+=:,./-]`)
 
 	if(pattern.MatchString(str)) {
-		fmt.Println("Easy bruh! No commands here!")
+		fmt.Println("No commands here!")
 		os.Exit(1)
 	}
 	return str
 }
 
-/* If no port number is specified after '-s',
- * start local server on port 8080.
- * If port number is specified after '-s',
- * start local server on given port.*/
+func home(rw http.ResponseWriter, req *http.Request) {
+	fmt.Println("Home!")
+}
+
+/* @@@(Biel A. P.): If no port number is 
+ * specified after '-s', start local 
+ * server on port 8080. If port number 
+ * is specified after '-s', start local 
+ * server on given port.*/
 
 func Start() {
 	if(len(os.Args) == 2) {
